7b3fbfbaa33c29c081ddad1a374d0234fa9ec229
RAMKI14 <ramki.t@hotmail.com>
2025-07-02 22:18:18 +0100

    test
    test

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "100px" }}>
+      <h1>🚀 Token Sale DApp</h1>
+      <ConnectButton />
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  getDefaultConfig,
+  RainbowKitProvider
+} from "@rainbow-me/rainbowkit";
+import {
+  WagmiProvider,
+  http
+} from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: "Token Crowdsale",
+  projectId: import.meta.env.VITE_WC_PROJECT_ID,
+  chains: [sepolia],
+  transports: {
+    [sepolia.id]: http()
+  }
+});
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+);
